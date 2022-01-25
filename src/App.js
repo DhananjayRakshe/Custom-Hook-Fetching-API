ca44f3a2538df04fab07330c2bee9b056ecf09db
Dhananjay <dhananjayrakshe77@gmail.com>
2022-01-25 23:46:11 +0530

    Second API added

--- src/App.js
+++ src/App.js
@@ -4,22 +4,51 @@ import './App.css'
 
 const App = () => {
   const [data] = useFetch("https://jsonplaceholder.typicode.com/todos");
+  const [wine_data] = useFetch("https://api.sampleapis.com/wines/reds");
 
   return (
     <div className='App'>
-      <h2>Custom Hook: useFetch() for fetching data from API</h2>
-      {
-        data &&
-          data.map((item) => {
-            return (
-              <>
-              
-              <p>User ID: {item.id}</p>
-              <p>Title: {item.title}</p>
-              </>
-            )
-          })
-      }
+
+      {/* User API */}
+      <div>
+        <h2>Custom Hook: useFetch() for fetching data from User API</h2>
+        <div className='user'>
+          {
+            data &&
+            data.map((item) => {
+              return (
+                <>
+
+                  <p>User ID: {item.id}</p>
+                  <p>Title: {item.title}</p>
+                  <hr />
+                </>
+              )
+            })
+          }
+        </div>
+      </div>
+
+      {/* Wine API */}
+      <div>
+        <h2>Custom Hook: useFetch() for fetching data from Wines API</h2>
+        <div className='user'>
+          {
+            wine_data &&
+            wine_data.map((item) => {
+              return (
+                <>
+
+                  <img className='wine_image' src={item.image} alt="" />
+                  <p>Wine Name: {item.winery}</p>
+                  <hr />
+                </>
+              )
+            })
+          }
+        </div>
+      </div>
+
     </div>
   );
 }
